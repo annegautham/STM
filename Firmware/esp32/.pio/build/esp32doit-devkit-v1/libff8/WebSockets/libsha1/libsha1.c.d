939764f272a0d7ae04413f3c0a261883cd7bee7c
Gautham Anne <annegautham@gmail.com>
2025-06-19 18:32:56 -0500

    firmware updates!

---
+++ Firmware/esp32/.pio/build/esp32doit-devkit-v1/libff8/WebSockets/libsha1/libsha1.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32doit-devkit-v1/libff8/WebSockets/libsha1/libsha1.c.o: \
+ .pio/libdeps/esp32doit-devkit-v1/WebSockets/src/libsha1/libsha1.c
---
+++ Firmware/esp32/include/secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// For WPA2 Enterprise (e.g., Northwestern University's eduroam-like network)
+#define EAP_WIFI_SSID "eduroam"      // The SSID of the WPA2-Enterprise network
+#define EAP_IDENTITY   "rnl2121@u.northwestern.edu"  // Anonymous identity (can sometimes be blank or same as username)
+#define EAP_USERNAME   "rnl2121@u.northwestern.edu"  // Your full enterprise username
+#define EAP_PASSWORD   "bund@IMSA12345"                // Your enterprise password
+
+#endif
